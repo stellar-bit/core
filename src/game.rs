a3a684ab42c1034f6085c742a654f6d026caa9fb
Patrik Cihal <patrik.cihal1234@gmail.com>
2023-05-06 17:05:16 +0200

    [sync]

--- src/game.rs
+++ src/game.rs
@@ -362,10 +362,11 @@ impl Game {
         sharp_body.velocity -= other_body.velocity;
         other_body.velocity = Vec2::ZERO;
 
-        // we rotate extra 1 to avoid vertical and horizontal lines
-        let sharp_points = sharp_body.bounds.clone().into_iter().map(|x| sharp_body.relative_to_world(x).rotate_rad(1.)).collect();
-        let other_points = other_body.bounds.clone().into_iter().map(|x| other_body.relative_to_world(x).rotate_rad(1.)).collect();
-        sharp_body.velocity = sharp_body.velocity.rotate_rad(1.);
+        // we rotate extra 1 (any number) to avoid vertical and horizontal lines
+        let extra_rot = 1.;
+        let sharp_points = sharp_body.bounds.clone().into_iter().map(|x| sharp_body.relative_to_world(x).rotate_rad(extra_rot)).collect();
+        let other_points = other_body.bounds.clone().into_iter().map(|x| other_body.relative_to_world(x).rotate_rad(extra_rot)).collect();
+        sharp_body.velocity = sharp_body.velocity.rotate_rad(extra_rot);
 
         check_sharp_collision(sharp_points, other_points, sharp_body.velocity, self.time_elapsed-cur_time).map(|(dt, sharp_point, other_line)| {
             CollisionInfo {
