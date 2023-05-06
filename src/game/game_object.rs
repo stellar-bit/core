ff9bee33afddc2dfd6dc2426f99aa6d2729d5d39
Patrik Cihal <patrik.cihal1234@gmail.com>
2023-05-06 17:15:01 +0200

    [sync]

--- src/game/game_object.rs
+++ src/game/game_object.rs
@@ -178,7 +178,7 @@ impl GameObjectBody {
         self.updated += 1;
     }
     pub fn update_fixed(&mut self, time: f32) {
-        assert!(time > self.cur_time);
+        assert!(time >= self.cur_time);
 
         let dt = time - self.cur_time;
 
