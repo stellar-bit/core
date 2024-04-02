afe61fb2f594c9868c5f777985813ed5e5916912
Patrik Cihal <patrik.cihal1234@gmail.com>
2024-04-02 15:51:35 +0200

    Larger random asteroids

--- src/game.rs
+++ src/game.rs
@@ -191,7 +191,7 @@ impl Game {
                     pos,
                     vel,
                     self.time_elapsed,
-                    self.rng.gen::<f32>().sqrt() * 20. + 2.,
+                    self.rng.gen::<f32>().sqrt() * 50. + 2.,
                     self.rng.gen(),
                 );
                 self.game_objects
