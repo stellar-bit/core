62dfdc348509e895353935f507d9f2f15f5e2d0d
Patrik Cihal <patrik.cihal1234@gmail.com>
2024-04-03 12:03:44 +0200

    Decrease health for components

--- src/game/spacecraft/component.rs
+++ src/game/spacecraft/component.rs
@@ -201,11 +201,11 @@ impl ComponentType {
     }
     pub fn health(&self) -> f32 {
         match &self {
-            ComponentType::Central => 2000.,
-            ComponentType::SteelBlock => 1000.,
-            ComponentType::RaptorEngine => 1500.,
-            ComponentType::KineticWeapon => 500.,
-            ComponentType::MissileLauncher => 1000.,
+            ComponentType::Central => 200.,
+            ComponentType::SteelBlock => 100.,
+            ComponentType::RaptorEngine => 150.,
+            ComponentType::KineticWeapon => 50.,
+            ComponentType::MissileLauncher => 100.,
         }
     }
     pub fn scale(&self) -> UVec2 {
