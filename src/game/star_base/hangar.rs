9ed11696907f819879bc131a04dbcdecefb2d4d8
Patrik Cihal <patrik.cihal1234@gmail.com>
2024-04-29 13:25:05 +0200

    Decrease build speed

--- src/game/star_base/hangar.rs
+++ src/game/star_base/hangar.rs
@@ -28,7 +28,7 @@ impl std::fmt::Display for Hangar {
 impl Hangar {
     pub fn new() -> Self {
         Self {
-            build_speed: 100.,
+            build_speed: 5.,
             progress: 0.,
             deploy: false,
             building_queue: VecDeque::new(),
